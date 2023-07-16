b52102d9aa44b2bc98bfd667502b84a3594e23f3
MatiasColazo1 <maty_colazo@hotmail.com>
2023-07-16 15:05:11 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,39 @@
+import { Component,  EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  
+  isDarkMode: boolean = false;
+  isSwitchOn: boolean = false;
+  isSidebarClosed: boolean = false;
+
+  toggleSwitch(): void {
+    this.isSwitchOn = !this.isSwitchOn;
+
+    if (this.isSwitchOn) {
+      this.closeSidebar();
+    } else {
+      this.openSidebar();
+    }
+  }
+
+  closeSidebar(): void {
+    this.isSidebarClosed = true;
+  }
+
+  openSidebar(): void {
+    this.isSidebarClosed = false;
+  }
+
+  toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+  }
+
+  getModeText(): string {
+    return this.isDarkMode ? 'Light Mode' : 'Dark Mode';
+  }
+}
