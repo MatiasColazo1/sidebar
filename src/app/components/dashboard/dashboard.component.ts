b3dc3b6f63951b61b33e6ad60acf99bdebe09fad
MatiasColazo1 <maty_colazo@hotmail.com>
2023-08-06 12:32:22 -0300

    ts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Ong, OngService } from 'src/app/services/ong.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+ //variable
+ ListaOng: Ong[] = [];
 
-  constructor() { }
+ constructor(private OngService: OngService, private router:Router) { }
 
-  ngOnInit(): void {
-  }
+ ngOnInit(): void {
+   this.listaOng();
+ }
+
+ listaOng()
+ {
+   this.OngService.getOng().subscribe(
+     res=>{
+       console.log(res)
+       this.ListaOng=<any>res;
+     },
+     err =>console.log(err)
+   );
+ }
+
+ eliminar(id:string)
+ {
+   this.OngService.deleteOng(id).subscribe(
+     res=>{
+       console.log('elemento Eliminado');
+     },
+     err=> console.log(err)
+   )
+ }
+
+ editar(id:string)
+ {
+   this.router.navigate(['/edit/'+id])
+ }
 
 }
